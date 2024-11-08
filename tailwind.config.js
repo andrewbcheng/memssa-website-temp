ef8bfe9df0eab4de156f3a07da03c77a25736fb9
Jaosn Chan <jlokshun@gmail.com>
2024-11-07 20:29:19 -0800

    change to export default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
     content: [
         './index.html',
         './src/**/*.{js,jsx,ts,tsx}',
@@ -37,4 +38,4 @@ module.exports = {
         },
     },
     plugins: [],
-};
+}
