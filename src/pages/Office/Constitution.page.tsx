96cd908a4ca7cdc3c74f41046e09f3f0bc63d6a7
Jaosn Chan <jlokshun@gmail.com>
2024-11-07 16:55:26 -0800

    basic layout for both pages done, need help with tailwind

--- src/pages/Office/Constitution.page.tsx
+++ src/pages/Office/Constitution.page.tsx
@@ -1,10 +1,23 @@
+import ConstitutionPDF from '../../assets/MEMSSA_Constitution.pdf'
+
 function Constitution() {
   return (
       // header here
       // page div
       <>
-        <div>
-          Constitution  
+        <div className="min-h-[90vh] mx-20 mt-48 flex flex-col gap-10 [&>*:last-child]:mb-40">
+          <header className="w-ful text-center">
+            <h1 className="text-red-50">Our Constitution</h1>
+          </header>
+          <div className="w-1/2 flex flex-center">
+            <iframe
+              src={ConstitutionPDF}
+              width="100%"
+              height="900px"
+              className="border-black rounded-md"
+              title="Constitution"
+            />
+          </div>
         </div>  
       </>
   );
--- src/pages/Resources/RSOResources.page.tsx
+++ src/pages/Resources/RSOResources.page.tsx
@@ -4,7 +4,30 @@ function RSOResources() {
         // page div
         <>
           <div>
-            <h1>RSO Leader Resources</h1>
+            <header>
+                <h1>RSO Leader Resources</h1>
+            </header>
+            {/* split btwn top and bottom*/}
+            <div id="resource-container">
+                {/* split btwn graphics and funding*/}
+                <div id="FundingR's" className="">
+                    <div>
+                        {/* graphics here (may need to add extra div for display pos) */}
+                    </div>
+                    <div>
+                        {/* Funding here (may need to add extra div for display pos) */}
+                    </div>
+                </div>
+                {/* split btwn booking and graphics*/}
+                <div id="BookingR's" className="">
+                    <div>
+                        {/* Booking here (may need to add extra div for display pos) */}
+                    </div>
+                    <div>
+                        {/* graphics here (may need to add extra div for display pos) */}
+                    </div>
+                </div>
+            </div>
           </div>  
         </>
     );
